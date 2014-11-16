e438b79c3459f506242d08c29acca1cdbd1573ce
Axel Ulmestig <axel_ulmestig@hotmail.com>
2014-11-16 23:48:02 +0100

    lagt till singleWildcardMatch och longerWildcardMatch

--- assignment1/Pattern.hs
+++ assignment1/Pattern.hs
@@ -1,5 +1,6 @@
 module Pattern where
 import Utilities
+import Data.List
 
 
 -------------------------------------------------------
@@ -15,15 +16,24 @@ substitute wildcard t s = concatMap (\list -> if list == wildcard then s else [l
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match wildcard [] _ = Just []
+match wildcard p [] = Nothing
+match wildcard p s = orElse
 match _ _ _ = Nothing
 {- TO BE WRITTEN -}
 
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-singleWildcardMatch (wc:ps) (x:xs) = Nothing
+singleWildcardMatch (wc:ps) (x:xs) = if(ps == xs) 
+										then Just [x]
+										else Nothing
+
 {- TO BE WRITTEN -}
-longerWildcardMatch (wc:ps) (x:xs) = Nothing
+longerWildcardMatch [] _ = Nothing
+longerWildcardMatch (x:xs) (y:ys) = if(f2 (x:xs) (y:ys)) then Just (f1 (x:xs) (y:ys)) else Nothing
+                    where   f1 (x:xs) ys = take (length ys - length xs) ys
+                            f2 (x:xs) ys = (drop (length ys - length xs) ys) == xs && length ys - length xs > 1
 {- TO BE WRITTEN -}
 
 
